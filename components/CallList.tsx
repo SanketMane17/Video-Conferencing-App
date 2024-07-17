9b4ea05d7c33d6b78f605e0c55ac16e1ed62d0b1
Sanket Mane <msanket450@gmail.com>
2024-07-17 23:36:35 +0530

    Fixed alignment

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -87,50 +87,54 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
     );
 
   return (
-    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+    <>
       {calls && calls.length > 0 ? (
-        calls.map((meeting: Call | CallRecording) => (
-          <MeetingCard
-            key={(meeting as Call).id || (meeting as CallRecording).filename}
-            icon={
-              type === "ended"
-                ? "/icons/previous.svg"
-                : type === "upcoming"
-                ? "/icons/upcoming.svg"
-                : "/icons/recordings.svg"
-            }
-            title={
-              (meeting as Call).state?.custom?.description ||
-              (meeting as CallRecording)?.filename?.substring(0, 20) ||
-              "Personal Meeting"
-            }
-            date={
-              (meeting as Call).state?.startsAt?.toLocaleString() ||
-              (meeting as CallRecording)?.start_time?.toLocaleString()
-            }
-            isPreviousMeeting={type === "ended"}
-            link={
-              type === "recordings"
-                ? (meeting as CallRecording).url
-                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
-                    (meeting as Call).id
-                  }`
-            }
-            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
-            buttonText={type === "recordings" ? "Play" : "Start"}
-            handleClick={
-              type === "recordings"
-                ? () => router.push(`${(meeting as CallRecording).url}`)
-                : () => router.push(`/meeting/${(meeting as Call).id}`)
-            }
-          />
-        ))
+        <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+          {calls.map((meeting: Call | CallRecording) => (
+            <MeetingCard
+              key={(meeting as Call).id || (meeting as CallRecording).filename}
+              icon={
+                type === "ended"
+                  ? "/icons/previous.svg"
+                  : type === "upcoming"
+                  ? "/icons/upcoming.svg"
+                  : "/icons/recordings.svg"
+              }
+              title={
+                (meeting as Call).state?.custom?.description ||
+                (meeting as CallRecording)?.filename?.substring(0, 20) ||
+                "Personal Meeting"
+              }
+              date={
+                (meeting as Call).state?.startsAt?.toLocaleString() ||
+                (meeting as CallRecording)?.start_time?.toLocaleString()
+              }
+              isPreviousMeeting={type === "ended"}
+              link={
+                type === "recordings"
+                  ? (meeting as CallRecording).url
+                  : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
+                      (meeting as Call).id
+                    }`
+              }
+              buttonIcon1={
+                type === "recordings" ? "/icons/play.svg" : undefined
+              }
+              buttonText={type === "recordings" ? "Play" : "Start"}
+              handleClick={
+                type === "recordings"
+                  ? () => router.push(`${(meeting as CallRecording).url}`)
+                  : () => router.push(`/meeting/${(meeting as Call).id}`)
+              }
+            />
+          ))}
+        </div>
       ) : (
         <div className="flex h-[60vh] w-full items-center justify-center">
           <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
